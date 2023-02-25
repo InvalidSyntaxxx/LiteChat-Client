459e0579fa959a2a96b519c6a3b0ca781b6893d8
Redamancy9189 <1791081494@qq.com>
2023-02-25 23:17:06 +0800

    update：修改小瑕疵

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,3 +1,11 @@
-export const DEFAULT_GROUP = '阿童木聊天室';
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 王远昭
+ * @Date: 2023-02-25 12:17:23
+ * @LastEditors: 王远昭
+ * @LastEditTime: 2023-02-25 22:33:14
+ */
+export const DEFAULT_GROUP = 'LiteChat聊天室';
 export const DEFAULT_BACKGROUND =
   'https://images.weserv.nl/?url=https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/453b8ebcdefa46a69c620da13f346ce2~tplv-k3u1fbpfcp-zoom-1.image?imageView2/2/w/800/q/85';
