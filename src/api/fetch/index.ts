91878bb52dd12aa8dc23744112177475b21af4f7
Redamancy9189 <1791081494@qq.com>
2023-02-25 15:04:14 +0800

    update: 服务器部署

--- src/api/fetch/index.ts
+++ src/api/fetch/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 王远昭
+ * @Date: 2023-02-25 12:17:23
+ * @LastEditors: 王远昭
+ * @LastEditTime: 2023-02-25 14:38:46
+ */
 import axios, { AxiosInstance } from 'axios';
 import { requestSuccess, requestFail, responseSuccess, responseFail } from './interceptors';
 import { socketUrl } from './config';
