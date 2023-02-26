b1f94482f81467c2e6917bee597171082bb12a70
Redamancy9189 <1791081494@qq.com>
2023-02-26 15:10:54 +0800

    feat：APP端

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: 王远昭
+ * @Date: 2023-02-25 12:17:23
+ * @LastEditors: 王远昭
+ * @LastEditTime: 2023-02-26 14:36:13
+ */
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 
@@ -12,7 +20,8 @@ const routes: Array<RouteConfig> = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash', // HbuilderX 打包APP空白屏
+  // mode: 'history', // 网页端
   base: process.env.BASE_URL,
   routes,
 });
